dcec6d3baa658611dce22cc4f551789d8f8e33f1
Antti Mäki <antti@anttimaki.com>
2022-02-02 10:41:51 +0200

    Working example with Babel-based compiler

--- packages/components/src/CookieReader.tsx
+++ packages/components/src/CookieReader.tsx
@@ -5,7 +5,7 @@ export const CookieReader: React.FC = () => {
   let cookie: string | undefined = "not read";
 
   // The next line works with Next.js v11, but breaks with v12.
-  // cookie = Cookies.get("someCookie");
+  cookie = Cookies.get("someCookie");
 
   return <p>Cookie value is: {cookie}</p>;
 };
--- packages/components/src/HookForm.tsx
+++ packages/components/src/HookForm.tsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 
 export const HookForm: React.FC = () => {
   // The next line works with Next.js v11, but breaks with v12.
-  // useForm();
+  useForm();
 
   return <form>
     <label>Please fill the form</label>
